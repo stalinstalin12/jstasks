cc1feb0e4ac4bd086224671d4dfbfd5b02f2e8b1
stalin <mstalin129@gmail.com>
2024-08-07 11:23:18 +0530

    gj

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let current= new Date();
+console.log(current);
+currentdate=document.getElementById('currentdate');
+currentdate=current;
+document.getElementById('currentdate').innerHTML=current;
+
+
